7bc54ac998fb96905911036eaa9babf366a7f78d
jaybhalani3011 <jaybhalani3011@gmail.com>
2022-02-09 16:54:57 +0530

    9th feb, fourth commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCL7VDmDGgFRnRrzho2H_r4XMtzJ9BloEw",
-    authDomain: "netflix-clone-3e017.firebaseapp.com",
-    projectId: "netflix-clone-3e017",
-    storageBucket: "netflix-clone-3e017.appspot.com",
-    messagingSenderId: "462615017076",
-    appId: "1:462615017076:web:7c24516035ea61e5330321"
+    apiKey:  process.env.REACT_APP_apiKey,// "AIzaSyCL7VDmDGgFRnRrzho2H_r4XMtzJ9BloEw" ||
+    authDomain:  process.env.REACT_APP_authDomain, // "netflix-clone-3e017.firebaseapp.com" ||
+    projectId:  process.env.REACT_APP_projectId, // "netflix-clone-3e017" ||
+    storageBucket:  process.env.REACT_APP_storageBucket, // "netflix-clone-3e017.appspot.com" ||
+    messagingSenderId:  process.env.REACT_APP_messagingSenderId, // "462615017076" ||
+    appId:  process.env.REACT_APP_appId // "1:462615017076:web:7c24516035ea61e5330321" ||
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
